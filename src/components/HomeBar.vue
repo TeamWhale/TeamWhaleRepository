b117e6fb74d6f38eea02128d2aa0ec4a454fab80
ShinyaKujirai <k.shin8824@gmail.com>
2021-03-15 14:50:29 +0900

    HomeBarの修正

--- src/components/HomeBar.vue
+++ src/components/HomeBar.vue
@@ -5,6 +5,51 @@
       <router-link to="/PostForm" class="nav__item nav__link">手作り</router-link>
       <router-link to="/singUp" class="nav__item nav__link">市販</router-link>
     </div>
+        <div class="search_bar">
+      <div class="search_box">
+        <input
+          id="search_input"
+          type="text"
+          placeholder="キーワードを入力"
+          v-model="search_keyword"
+          required
+        />
+        <img
+          v-on:click="search"
+          id="search_icon"
+          src="../assets/search_mark.png"
+        />
+      </div>
+    </div>
+    <div class="recipes">
+      <div class="tab_items">
+        <div class="tab_item current"><a>すべて</a></div>
+        <div class="tab_item"><a>手作り</a></div>
+        <div class="tab_item"><a>市販</a></div>
+      </div>
+      <div class="recom">
+        <h2>今日のおすすめ</h2>
+        <div class="recom_items">
+          <div class="recom_item">
+            <div class="recom_description">
+              <div class="recom_name">卵焼き</div>
+              <div class="recom_easy">お手軽：★★★☆☆</div>
+              <div class="recom_time">時間：10分</div>
+            </div>
+          </div>
+          <div class="recom_item">
+            <div class="recom_description">
+              <div class="recom_name">カレーライス</div>
+            </div>
+          </div>
+          <div class="recom_item">
+            <div class="recom_description">
+              <div class="recom_name">味の素「ザ★チャーハン」</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
