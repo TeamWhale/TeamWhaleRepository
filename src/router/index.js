93396c1b2c5b2a9cab055828b23f7c68d334a8d5
saaaki527 <saaaki527@gmail.com>
2021-03-19 19:50:39 +0900

    added SearchResult.vue and edited router

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,9 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import PostForm from "../views/PostForm.vue";
 import MyPage from "../views/MyPage.vue";
-import firebase from "firebase"
-import "firebase/auth"
+import SearchResult from "../views/SearchResult.vue";
+import firebase from "firebase";
+import "firebase/auth";
 
 Vue.use(VueRouter);
 
@@ -15,32 +16,37 @@ const routes = [
     component: Home,
   },
   {
+    path: "/SearchResult",
+    name: "SearchResult",
+    component: SearchResult,
+  },
+  {
     path: "/PostForm",
     name: "PostForm",
     component: PostForm,
     beforeEnter: (to, from, next) => {
-      firebase.auth().onAuthStateChanged(user => {
-        if(user) {
-          next()
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          next();
         } else {
           window.alert("ログインが必要です");
         }
       });
-    }
+    },
   },
   {
     path: "/MyPage",
     name: "MyPage",
     component: MyPage,
-    beforeEnter: (to, from, next) =>{
-      firebase.auth().onAuthStateChanged(user =>{
-        if(user){
-          next()
+    beforeEnter: (to, from, next) => {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          next();
         } else {
           window.alert("ログインが必要です");
         }
-      })
-    }
+      });
+    },
   },
 ];
 
