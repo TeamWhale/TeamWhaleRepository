2b1e96b0cd413243d25781de621fd5a61e4180ff
saaaki527 <saaaki527@gmail.com>
2021-03-20 16:50:14 +0900

    designed postform

--- src/views/PostForm.vue
+++ src/views/PostForm.vue
@@ -1,131 +1,208 @@
 <template>
   <div class="post__wrapper">
+    <Search />
     <h1>投稿</h1>
-    <div>
-      <button @click="make">手作り</button>
-      <button @click="made">市販</button>
-    </div>
+    <div class="post__box">
+      <div class="form-switchtabs">
+        <div
+          @click="make"
+          class="form-switchtab"
+          v-bind:class="{ form_active: isActiveA }"
+        >
+          手作り
+        </div>
+        <div
+          @click="made"
+          class="form-switchtab"
+          v-bind:class="{ form_active: isActiveB }"
+        >
+          市販
+        </div>
+      </div>
       <!-- 手作り投稿 -->
-    <div v-if="cook">
-      <h2>手作り</h2>
-      <form @submit.prevent="makePostForm">
-        <!-- 料理名 -->
-      <div class="title">
-        <input v-model="title" type="text" placeholder="料理タイトル" class="input-rec">
-      </div>
-      <!-- 調理時間 -->
-      <div>
-        <h2>調理時間</h2>
-        <div>
-          <select name="time" v-model="selected">
-            <option disabled value="" class="input-rec">選択してください</option>
-            <option v-for="option in options" :key="option.id">
-             {{option.name}}
-            </option>
-          </select>
-        </div>
-      </div>
-      <br>
-      <!-- 写真 -->
-      <div>
-        <input type="file" accept="image/*" @change="onImageUploadedMake($event)">
-        <img :src="imageURL" alt="料理の写真" width="300" height="200">
-      </div>
-      <!-- 紹介文 -->
-      <div class="introduce">
-        <textarea v-model="introduce" id="" cols="30" rows="10" placeholder="紹介文"></textarea>
-      </div>
-        <!-- 材料 -->
-        <div class="ingredients">
-          <h2>材料</h2>
-          <div v-for="(newIngredient, index) in newIngredients" :key="index">
+      <div v-if="cook">
+        <form @submit.prevent="makePostForm">
+          <!-- 料理名 -->
+          <div class="form_title form_item">
+            <h3>レシピのタイトル</h3>
             <input
-              v-model="newIngredient.name"
+              v-model="title"
               type="text"
-              placeholder="玉ねぎ"
-              class="input-rec"
+              placeholder="料理タイトル"
+              class="input_rec"
             />
-            <input
-              v-model="newIngredient.amount"
-              type="text"
-              placeholder="一個"
-              class="input-rec"
-            />
-            <button @click.prevent="removeIngredients(index)">削除</button>
           </div>
-          <button @click.prevent="addIngredients">材料を追加する</button>
-        </div>
-        <!-- 作り方 -->
-        <div class="howTo">
-          <h2>作り方</h2>
-          <div v-for="(newHowTo, index) in newHowTos" :key="index">
-            {{ index + 1 }}
-            <textarea
-              v-model="newHowTo.text"
-              cols="30"
-              rows="5"
-              placeholder="作り方"
-              class="input-rec"
-            ></textarea>
-            <button @click.prevent="removeNewHowTos(index)">削除</button>
+          <!-- 2nd block -->
+          <div class="form_2 form_item">
+            <!-- 2nd block left -->
+            <!-- 写真 -->
+            <div class="form__pic">
+              <input
+                type="file"
+                accept="image/*"
+                @change="onImageUploadedMake($event)"
+                class="pic__upload"
+              />
+              <img :src="imageURL" alt="料理の写真" width="300" height="200" />
+            </div>
+            <!-- 2nd block right -->
+            <div class="form_2right">
+              <!-- 調理時間 -->
+              <div class="form_time">
+                <h3>調理時間</h3>
+                <div>
+                  <select
+                    name="time"
+                    v-model="selected"
+                    class="form_time_choice"
+                  >
+                    <option disabled value="">選択してください</option>
+                    <option v-for="option in options" :key="option.id">
+                      {{ option.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+              <!-- 紹介文 -->
+              <div class="introduce">
+                <h3>紹介文</h3>
+                <textarea
+                  v-model="introduce"
+                  id=""
+                  cols="30"
+                  rows="5"
+                  placeholder="キャッチコピーや料理の際のコツ・ポイントを入力"
+                  class="input_rec"
+                ></textarea>
+              </div>
+            </div>
           </div>
-          <button @click.prevent="addNewHowTos">作り方を追加する</button>
-        </div>
+          <!-- 材料 -->
+          <div class="ingredients form_item">
+            <h3>材料</h3>
+            <div v-for="(newIngredient, index) in newIngredients" :key="index">
+              <div class="form_horizontal">
+                <input
+                  v-model="newIngredient.name"
+                  type="text"
+                  placeholder="（例）玉ねぎ"
+                  class="input_rec input_zairyo"
+                />
+                <input
+                  v-model="newIngredient.amount"
+                  type="text"
+                  placeholder="（例）一個"
+                  class="input_rec input_amount"
+                />
+                <img
+                  src="../assets/delete-button.png"
+                  @click.prevent="removeIngredients(index)"
+                  class="form_delete"
+                />
+              </div>
+            </div>
+            <button @click.prevent="addIngredients" class="add_input">
+              材料を追加する
+            </button>
+          </div>
+          <!-- 作り方 -->
+          <div class="form_item">
+            <h3>作り方</h3>
 
-        <!-- 投稿ボタン -->
-        <div>
-          <button type="submit" class="make-button">投稿</button>
-        </div>
-      </form>
-    </div>
+            <div
+              v-for="(newHowTo, index) in newHowTos"
+              :key="index"
+              class="form_horizontal"
+            >
+              <div class="howto_num">{{ index + 1 }}</div>
+              <textarea
+                v-model="newHowTo.text"
+                cols="25"
+                rows="2"
+                placeholder="作り方を入力"
+                class="input_rec"
+              ></textarea>
+              <img
+                src="../assets/delete-button.png"
+                @click.prevent="removeNewHowTos(index)"
+                class="form_delete"
+              />
+            </div>
+            <button @click.prevent="addNewHowTos" class="add_input">
+              作り方を追加する
+            </button>
+          </div>
+
+          <!-- 投稿ボタン -->
+          <div>
+            <button type="submit" class="make-button">投稿</button>
+          </div>
+        </form>
+      </div>
 
       <!-- 市販投稿 -->
-    <div v-if="cooked">
-      <h2>市販</h2>
-      <form @submit.prevent="madePostForm">
-        <!-- タイトル -->
-        <div class="title">
-          <input v-model="title" type="text" placeholder="料理タイトル">
-        </div>
-        <!-- 写真 -->
-        <div>
-          <input type="file" accept="image/*" @change="onImageUploadedMade($event)">
-          <img :src="imageURL" alt="料理の写真" width="300" height="200">
-        </div>
-        <!-- 紹介文 -->
-        <div class="introduce">
-          <textarea v-model="introduce" cols="30" rows="10" placeholder="紹介文"></textarea>
-        </div>
-        <!-- 投稿ボタン -->
-        <div>
-          <button type="submit" class="make-button">投稿</button>
-        </div>
-      </form>
+      <div v-if="cooked">
+        <form @submit.prevent="madePostForm" class="form_wrapper">
+          <!-- タイトル -->
+          <div class="form_title form_item">
+            <h3>レシピのタイトル</h3>
+            <input
+              v-model="title"
+              type="text"
+              placeholder="料理タイトル"
+              class="input_rec"
+            />
+          </div>
+          <!-- 写真 -->
+          <div class="form_pic">
+            <input
+              type="file"
+              accept="image/*"
+              @change="onImageUploadedMade($event)"
+              class="pic__upload"
+            />
+            <img :src="imageURL" alt="料理の写真" width="300" height="200" />
+          </div>
+          <!-- 紹介文 -->
+          <div class="introduce form_item">
+            <h3>紹介文</h3>
+            <textarea
+              v-model="introduce"
+              cols="30"
+              rows="10"
+              placeholder="紹介文"
+              class="input_rec"
+            ></textarea>
+          </div>
+          <!-- 投稿ボタン -->
+          <div>
+            <button type="submit" class="make-button">投稿</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import firebase from "firebase"
-import "firebase/firestore"
-import "firebase/auth"
-import "firebase/storage"
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+import Search from "../views/Search.vue";
+
 export default {
   data() {
     return {
-      cook: false,
+      cook: true,
       cooked: false,
       title: "",
       selected: "",
       imageName: "",
       imageURL: "",
       introduce: "",
-      newIngredients: [
-        {name: "", amount: ""}
-        ],
-      newHowTos: [
-        {text: ""}
-        ],
+      newIngredients: [{ name: "", amount: "" }],
+      newHowTos: [{ text: "" }],
       options: [
         { id: 1, name: "５分以内" },
         { id: 2, name: "約１０分" },
@@ -134,43 +211,49 @@ export default {
         { id: 5, name: "約１時間" },
         { id: 6, name: "1時間以上" },
       ],
-      
-
-      
+      isActiveA: true,
+      isActiveB: false,
     };
+  },
+  components: {
+    Search,
   },
   methods: {
     make() {
       this.cook = true;
       this.cooked = false;
+      this.isActiveA = true;
+      this.isActiveB = false;
     },
     made() {
       this.cooked = true;
       this.cook = false;
+      this.isActiveA = false;
+      this.isActiveB = true;
     },
-    addIngredients(){
+    addIngredients() {
       this.newIngredients.push({
         name: "",
-        amount: ""
-      })
+        amount: "",
+      });
     },
-    removeIngredients(index){
-      this.newIngredients.splice(index, 1)
+    removeIngredients(index) {
+      this.newIngredients.splice(index, 1);
     },
-    addNewHowTos(){
+    addNewHowTos() {
       this.newHowTos.push({
-        text: ""
-      })
+        text: "",
+      });
     },
-    removeNewHowTos(index){
-      this.newHowTos.splice(index, 1)
+    removeNewHowTos(index) {
+      this.newHowTos.splice(index, 1);
     },
     // makeの写真投稿メソッド
-    onImageUploadedMake(e){
-      const image = e.target.files[0]
-      this.createImageMake(image)
+    onImageUploadedMake(e) {
+      const image = e.target.files[0];
+      this.createImageMake(image);
     },
-    createImageMake(image){
+    createImageMake(image) {
       // const render = new FileReader()
       // render.readAsDataURL(image)
       // render.onload = () =>{
@@ -180,26 +263,23 @@ export default {
       const storageRef = firebase.storage().ref();
       const createdAt = new Date();
       const timestamp = createdAt.getTime();
-      const uniqueFileName = timestamp + "_" + image.name
-      const fileRef = storageRef.child("images/" + uniqueFileName)
-      fileRef.put(image)
-      .then(snapshot =>{
-        snapshot.ref.getDownloadURL()
-        .then(url =>{
-          console.log(url)
-          console.log(image.name)
-          this.imageURL = url
-          this.imageName = image.name
-        })
-      })
-
+      const uniqueFileName = timestamp + "_" + image.name;
+      const fileRef = storageRef.child("images/" + uniqueFileName);
+      fileRef.put(image).then((snapshot) => {
+        snapshot.ref.getDownloadURL().then((url) => {
+          console.log(url);
+          console.log(image.name);
+          this.imageURL = url;
+          this.imageName = image.name;
+        });
+      });
     },
     // madeの写真投稿メソッド
-    onImageUploadedMade(e){
-      const image = e.target.files[0]
-      this.createImageMade(image)
+    onImageUploadedMade(e) {
+      const image = e.target.files[0];
+      this.createImageMade(image);
     },
-    createImageMade(image){
+    createImageMade(image) {
       // const render = new FileReader()
       // render.readAsDataURL(image)
       // render.onload = () =>{
@@ -209,78 +289,188 @@ export default {
       const storageRef = firebase.storage().ref();
       const createdAt = new Date();
       const timestamp = createdAt.getTime();
-      const uniqueFileName = timestamp + "_" + image.name
-      const fileRef = storageRef.child("images/" + uniqueFileName)
-      fileRef.put(image)
-      .then(snapshot =>{
-        snapshot.ref.getDownloadURL()
-        .then(url =>{
-          console.log(url)
-          console.log(image.name)
-          this.imageURL = url
-          this.imageName = image.name
-        })
-      })
+      const uniqueFileName = timestamp + "_" + image.name;
+      const fileRef = storageRef.child("images/" + uniqueFileName);
+      fileRef.put(image).then((snapshot) => {
+        snapshot.ref.getDownloadURL().then((url) => {
+          console.log(url);
+          console.log(image.name);
+          this.imageURL = url;
+          this.imageName = image.name;
+        });
+      });
     },
     // firebaseに保存
-    makePostForm(){
+    makePostForm() {
       // ログインしているユーザーの uidを取得
-      firebase.auth().onAuthStateChanged(user =>{
-        if(user){
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
           //firestoreのrecipeにdataと取得したuidを保存
-          firebase.firestore().collection("recipe").add({
-            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-            type: "手作り",
-            title: this.title,
-            selected: this.selected,
-            imageName: this.imageName,
-            imageURL: this.imageURL,
-            introduce: this.introduce,
-            newIngredients: this.newIngredients,
-            newHowTo: this.newHowTos,
-            uid: user.uid
-          })
+          firebase
+            .firestore()
+            .collection("recipe")
+            .add({
+              createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+              type: "手作り",
+              title: this.title,
+              selected: this.selected,
+              imageName: this.imageName,
+              imageURL: this.imageURL,
+              introduce: this.introduce,
+              newIngredients: this.newIngredients,
+              newHowTo: this.newHowTos,
+              uid: user.uid,
+            });
         }
-      })
+      });
     },
-    madePostForm(){
+    madePostForm() {
       // ログインしているユーザーの uidを取得
-      firebase.auth().onAuthStateChanged(user =>{
-        if(user){
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
           //firestoreのrecipeにdataと取得したuidを保存
-          firebase.firestore().collection("recipe").add({
-           createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-           type: "市販",
-           title: this.title,
-           imageName: this.imageName,
-           imageURL: this.imageURL,
-           introduce: this.introduce,
-           uid: user.uid
-          })
+          firebase
+            .firestore()
+            .collection("recipe")
+            .add({
+              createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+              type: "市販",
+              title: this.title,
+              imageName: this.imageName,
+              imageURL: this.imageURL,
+              introduce: this.introduce,
+              uid: user.uid,
+            });
         }
-      })
+      });
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
 .post__wrapper {
+  height: 1500px;
   text-align: center;
+  background-color: #f6f6f6;
 }
-.input-rec {
+.post__box {
+  width: 700px;
+  height: 1000px;
+  background-color: #fff;
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 100px;
+}
+.form-switchtabs {
+  display: flex;
+}
+.form-switchtab {
+  width: 350px;
+  height: 50px;
+  padding-top: 13px;
+  border: none;
+  background-color: #fafafa;
+}
+.form_active {
+  background-color: #fff;
+}
+.form-switchtab:hover {
+  cursor: pointer;
+}
+.form_item {
+  margin-left: 45px;
+  margin-right: 45px;
+}
+.input_rec {
+  border-style: solid;
+  width: 100%;
+  padding: 10px;
   font-family: inherit;
   border-color: #ececec;
   border-radius: 5px;
   border-width: 2px;
-  padding: 10px;
 }
-.input-rec:focus {
-  border: #fff;
+.input_rec:focus {
+  border-style: none;
   /* なんか効かない... */
+}
+textarea {
+  resize: none;
+}
+.form_title {
+  width: 400px;
+  margin-top: 30px;
 }
 button:hover {
   cursor: pointer;
+}
+h3 {
+  text-align: left;
+  font-size: 18px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+.form_2 {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.form__pic {
+  display: flex;
+  flex-direction: column;
+  margin-top: 15px;
+}
+.pic__upload {
+  width: 270px;
+}
+.form_2right {
+  /* margin-right: 45px; */
+}
+.form_time {
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+.form_horizontal {
+  display: flex;
+}
+.form_time_choice {
+  margin-top: 12px;
+  margin-left: 20px;
+}
+.input_zairyo {
+  width: 400px;
+  margin-right: 5px;
+}
+.input_amount {
+  width: 280px;
+}
+.howto_num {
+  font-size: 24px;
+  margin-right: 10px;
+}
+.form_delete {
+  width: 30px;
+  height: 30px;
+  border: none;
+  border-radius: 3px;
+  margin-top: 5px;
+  margin-left: 5px;
+}
+.form_delete:hover {
+  cursor: pointer;
+}
+.add_input {
+  width: 150px;
+  height: 30px;
+  color: #ff9900;
+  border-style: solid;
+  background-color: #ffffff;
+  border-color: #ff9900;
+  border-radius: 3px;
+  border-width: 1px;
+  margin-top: 5px;
 }
 .make-button {
   width: 400px;
